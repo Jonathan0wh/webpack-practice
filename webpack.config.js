82271d1687b10502d00bc547859ff368246bfe7c
Jonathan <jonathan0wh@gmail.com>
2017-09-18 17:16:20 +1000

    Init

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/
+      },
+      {
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        }),
+        test: /\.css$/
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ]
+};
+
+module.exports = config;
