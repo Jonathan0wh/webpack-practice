1e77059f64db7f4d3cbd4f9f4b970b22155b9b8c
Jonathan <jonathan0wh@gmail.com>
2017-09-21 15:16:16 +1000

    Add image loader

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -1,6 +1,12 @@
+import big from '../assets/big.jpeg';
+import small from '../assets/small.jpeg';
 import './styles/image_viewer.css';
 
 const image = document.createElement('img');
-image.src = 'https://lorempixel.com/400/400';
+image.src = small;
+
+const bigImage = document.createElement('img');
+bigImage.src = big;
 
 document.body.appendChild(image);
+document.body.appendChild(bigImage);
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,39 @@
 const path = require("path");
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const config = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "build"),
-    filename: "bundle.js"
+    filename: "bundle.js",
+    publicPath: 'build/'
   },
   module: {
     rules: [
       {
-        use: 'babel-loader',
-        test: /\.js$/
+        test: /\.js$/,
+        use: "babel-loader"
       },
       {
+        test: /\.css$/,
         use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader'
-        }),
-        test: /\.css$/
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 40000 }
+          },
+          'image-webpack-loader'
+        ]
       }
     ]
   },
-  plugins: [
-    new ExtractTextPlugin('style.css')
-  ]
+  plugins: [new ExtractTextPlugin("style.css")]
 };
 
 module.exports = config;
